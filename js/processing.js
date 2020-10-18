a07069138386eb30effeb38d2c5b3d90ac48db03
lwoh <lwoh@users.noreply.github.com>
2020-10-18 16:58:38 +0800

    Update processing.js

--- js/processing.js
+++ js/processing.js
@@ -24,67 +24,56 @@ function getHashParams() {
     return hashParams;
 };
 
-$(document).ready(function() 
-{
-console.log("z");
-var params = getHashParams();
-console.log("z2");
-var access_token = params.access_token,
-state = params.state,
-storedState = localStorage.getItem(stateKey);
-console.log("z3");
-if (acces_token)
-{
- if (state == null || state !== storedState) {
- storedState = localStorage.getItem(stateKey);
-    alert('There was an error during the authentication');
-} else {
-	localStorage.removeItem(stateKey);
-    if (access_token) {
-	    $.ajax({
-            url: 'https://api.spotify.com/v1/me',
-            headers: {
-                'Authorization': 'Bearer ' + access_token
-            },
-            success: function(response) {
-            //userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+$(document).ready(function(){
+  var params = getHashParams();
+  var access_token = params.access_token;
+    
+  if (access_token)
+  {
+    var state = params.state;
+    var storedState = localStorage.getItem(stateKey);
+    
+	if (state == null || state !== storedState) {
+      alert('There was an error during the authentication');
+    } 
+	else {
+	  localStorage.removeItem(stateKey);
+      
+	  $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+             'Authorization': 'Bearer ' + access_token
+        },
+        success: function(response) {
+        //userProfilePlaceholder.innerHTML = userProfileTemplate(response);
 
-            $("#btn-login").hide();
-            //$('#loggedin').show();
-		    //console.log('A')
-            }
-        });
-   }
-   else {
-    console.log('B')
-    //$('#login').show();
-    //$('#loggedin').hide();
-}
-}
-}
-		  
-//document.getElementById('btn-login').addEventListener('click', function() {
-
+        $("#btn-login").hide();
+        //$('#loggedin').show();
+		//console.log('A')
+        }
+      });
+    }
+  }
     $("#btn-login").click(function(){
-        var client_id = 'fa4b3d7996a14a72985e572a258ebdcf'; // Your client id
-        var redirect_uri = 'https://lwoh.github.io/dvider/'; // Your redirect uri
+	//document.getElementById('login-button').addEventListener('click', function() {
 
-        var state = generateRandomString(16);
-        localStorage.setItem(stateKey, state);
-		
-		storedState = localStorage.getItem(stateKey);
-		
-        var scope = 'user-read-private user-read-email';
+      var client_id = 'fa4b3d7996a14a72985e572a258ebdcf'; // Your client id
+      var redirect_uri = 'https://lwoh.github.io/dvider/'; // Your redirect uri
 
-        var url = 'https://accounts.spotify.com/authorize';
-		url += '?client_id=' + encodeURIComponent(client_id);
-        url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-		url += '&scope=' + encodeURIComponent(scope); 
-		url += '&response_type=token';		
-		url += '&state=' + encodeURIComponent(state);   
+      var state = generateRandomString(16);
+      localStorage.setItem(stateKey, state);
+		
+	  var scope = 'user-read-private user-read-email';
+
+      var url = 'https://accounts.spotify.com/authorize';
+      url += '?client_id=' + encodeURIComponent(client_id);
+      url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+	  url += '&scope=' + encodeURIComponent(scope); 
+	  url += '&response_type=token';		
+	  url += '&state=' + encodeURIComponent(state);   
         
-        window.location = url;
-		
-   }), false
-});
+      window.location = url;
+	});	
+    //}, false);
+ });
  
