2dd8a70394e42f1081182552ebba02f3e39c82bc
lwoh <lwoh@users.noreply.github.com>
2020-10-22 00:36:09 +0800

    Update processing.js

--- js/processing.js
+++ js/processing.js
@@ -24,6 +24,48 @@ function getHashParams() {
     return hashParams;
 };
 
+var arrName = ["Playlist 1", "Playlist 2", "Playlist 3"];
+var arrDesc = ["Desc 1", "Desc 2", "Desc 3"];
+
+function populatePlaylist(arrName, arrDesc)
+{
+  var strHash = "#"
+  var listPlaylist = document.getElementById("list-playlist");
+
+  for (var i = 0; i < arrName.length; i++) 
+  {
+     var a = document.createElement("a");
+     a.setAttribute("href", "#");
+     a.setAttribute("class", "list-group-item list-group-item-action flex-column align-items-start active");
+
+     var div = document.createElement("div");
+     div.setAttribute("class", "d-flex w-100 justify-content-between");
+    
+     var count = document.createElement("small");
+     count.innerHTML = strHash + i;
+     
+     var hTitle = document.createElement("h5");
+     hTitle.setAttribute("class", "mb-1");
+     hTitle.innerHTML = arrName[i];
+     
+     var pDesc = document.createElement("p");
+     pDesc.setAttribute("class", "mb-1");
+     pDesc.innerHTML = arrDesc[i];
+
+     var addInfo = document.createElement("small"); 
+     addInfo.innerHTML = "additional info";
+    
+     a.appendChild(div);
+     div.appendChild(count);
+     div.appendChild(hTitle);
+     
+     a.appendChild(pDesc);
+     a.appendChild(addInfo);   
+
+     listPlaylist.appendChild(a);	 
+  }
+};
+
 $(document).ready(function(){
   var params = getHashParams();
   var access_token = params.access_token;
@@ -46,13 +88,32 @@ $(document).ready(function(){
         },
         success: function(response) {
         //userProfilePlaceholder.innerHTML = userProfileTemplate(response);
-
-        $("#btn-login").hide();
+        
+		$("#main-jumbotron").hide();
+		$("#main-home-left").show();
+	    $("#main-home-right").show();
+        //$("#btn-login").hide();
+		
+		//$("#nav-home").show();
+	    //$("#nav-player").show();
+	    //$("#nav-profile").show();
+		
         //$('#loggedin').show();
 		//console.log('A')
         }
       });
     }
+  }
+  else
+  {
+    $("#main-jumbotron").show();
+	$("#main-home-left").hide();
+	$("#main-home-right").hide();
+	
+	//populatePlaylist(arrName, arrDesc);
+    //$("#nav-home").hide();
+	//$("#nav-player").hide();
+	//$("#nav-profile").hide();
   }
     $("#btn-login").click(function(){
 	//document.getElementById('login-button').addEventListener('click', function() {
